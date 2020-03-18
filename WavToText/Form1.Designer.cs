ec47b34cd5e72009392a4b5b0d5c7fad062f4e3a
ERNDS <ernds.glts@gmail.com>
2020-03-18 08:38:55 +0530

    Base code added.
    Base code added.

---
+++ WavToText/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WavToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_lblWavFileLocaltion = new System.Windows.Forms.Label();
+            this.m_txtBoxWavFileURL = new System.Windows.Forms.TextBox();
+            this.m_btnSelectFile = new System.Windows.Forms.Button();
+            this.m_txtBoxLoopTime = new System.Windows.Forms.TextBox();
+            this.m_lblLoopTime = new System.Windows.Forms.Label();
+            this.m_txtBoxWriteToTextURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.m_lblLanguage = new System.Windows.Forms.Label();
+            this.m_cmbxLanguage = new System.Windows.Forms.ComboBox();
+            this.m_btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // m_lblWavFileLocaltion
+            // 
+            this.m_lblWavFileLocaltion.AutoSize = true;
+            this.m_lblWavFileLocaltion.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_lblWavFileLocaltion.Location = new System.Drawing.Point(30, 34);
+            this.m_lblWavFileLocaltion.Name = "m_lblWavFileLocaltion";
+            this.m_lblWavFileLocaltion.Size = new System.Drawing.Size(236, 21);
+            this.m_lblWavFileLocaltion.TabIndex = 0;
+            this.m_lblWavFileLocaltion.Text = "Read from wave file location(url)";
+            // 
+            // m_txtBoxWavFileURL
+            // 
+            this.m_txtBoxWavFileURL.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_txtBoxWavFileURL.Location = new System.Drawing.Point(33, 58);
+            this.m_txtBoxWavFileURL.Multiline = true;
+            this.m_txtBoxWavFileURL.Name = "m_txtBoxWavFileURL";
+            this.m_txtBoxWavFileURL.Size = new System.Drawing.Size(281, 64);
+            this.m_txtBoxWavFileURL.TabIndex = 1;
+            // 
+            // m_btnSelectFile
+            // 
+            this.m_btnSelectFile.Location = new System.Drawing.Point(320, 75);
+            this.m_btnSelectFile.Name = "m_btnSelectFile";
+            this.m_btnSelectFile.Size = new System.Drawing.Size(75, 30);
+            this.m_btnSelectFile.TabIndex = 2;
+            this.m_btnSelectFile.Text = "...";
+            this.m_btnSelectFile.UseVisualStyleBackColor = true;
+            this.m_btnSelectFile.Click += new System.EventHandler(this.m_btnSelectFile_Click);
+            // 
+            // m_txtBoxLoopTime
+            // 
+            this.m_txtBoxLoopTime.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.m_txtBoxLoopTime.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_txtBoxLoopTime.Location = new System.Drawing.Point(33, 149);
+            this.m_txtBoxLoopTime.Name = "m_txtBoxLoopTime";
+            this.m_txtBoxLoopTime.Size = new System.Drawing.Size(281, 29);
+            this.m_txtBoxLoopTime.TabIndex = 4;
+            this.m_txtBoxLoopTime.Text = "5";
+            // 
+            // m_lblLoopTime
+            // 
+            this.m_lblLoopTime.AutoSize = true;
+            this.m_lblLoopTime.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_lblLoopTime.Location = new System.Drawing.Point(30, 125);
+            this.m_lblLoopTime.Name = "m_lblLoopTime";
+            this.m_lblLoopTime.Size = new System.Drawing.Size(228, 21);
+            this.m_lblLoopTime.TabIndex = 3;
+            this.m_lblLoopTime.Text = "Read wav file loop time(sec/10)";
+            // 
+            // m_txtBoxWriteToTextURL
+            // 
+            this.m_txtBoxWriteToTextURL.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_txtBoxWriteToTextURL.Location = new System.Drawing.Point(33, 220);
+            this.m_txtBoxWriteToTextURL.Multiline = true;
+            this.m_txtBoxWriteToTextURL.Name = "m_txtBoxWriteToTextURL";
+            this.m_txtBoxWriteToTextURL.Size = new System.Drawing.Size(281, 62);
+            this.m_txtBoxWriteToTextURL.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Write to text file location(url)";
+            // 
+            // m_lblLanguage
+            // 
+            this.m_lblLanguage.AutoSize = true;
+            this.m_lblLanguage.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_lblLanguage.Location = new System.Drawing.Point(30, 295);
+            this.m_lblLanguage.Name = "m_lblLanguage";
+            this.m_lblLanguage.Size = new System.Drawing.Size(78, 21);
+            this.m_lblLanguage.TabIndex = 7;
+            this.m_lblLanguage.Text = "Language";
+            // 
+            // m_cmbxLanguage
+            // 
+            this.m_cmbxLanguage.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_cmbxLanguage.FormattingEnabled = true;
+            this.m_cmbxLanguage.Items.AddRange(new object[] {
+            "Chinese (China)",
+            "Chinese (Taiwan)",
+            "English (United Kingdom)",
+            "English (United States)",
+            "French (France)",
+            "German (Germany)",
+            "Japanese (Japan)",
+            "Spanish (Spain)"});
+            this.m_cmbxLanguage.Location = new System.Drawing.Point(34, 325);
+            this.m_cmbxLanguage.Name = "m_cmbxLanguage";
+            this.m_cmbxLanguage.Size = new System.Drawing.Size(280, 29);
+            this.m_cmbxLanguage.Sorted = true;
+            this.m_cmbxLanguage.TabIndex = 0;
+            this.m_cmbxLanguage.SelectedIndexChanged += new System.EventHandler(this.m_cmbxLanguage_SelectedIndexChanged);
+            // 
+            // m_btnSave
+            // 
+            this.m_btnSave.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_btnSave.Location = new System.Drawing.Point(320, 398);
+            this.m_btnSave.Name = "m_btnSave";
+            this.m_btnSave.Size = new System.Drawing.Size(75, 30);
+            this.m_btnSave.TabIndex = 9;
+            this.m_btnSave.Text = "Ok";
+            this.m_btnSave.UseVisualStyleBackColor = true;
+            this.m_btnSave.Click += new System.EventHandler(this.m_btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 450);
+            this.Controls.Add(this.m_btnSave);
+            this.Controls.Add(this.m_cmbxLanguage);
+            this.Controls.Add(this.m_lblLanguage);
+            this.Controls.Add(this.m_txtBoxWriteToTextURL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.m_txtBoxLoopTime);
+            this.Controls.Add(this.m_lblLoopTime);
+            this.Controls.Add(this.m_btnSelectFile);
+            this.Controls.Add(this.m_txtBoxWavFileURL);
+            this.Controls.Add(this.m_lblWavFileLocaltion);
+            this.Name = "Form1";
+            this.Text = " Speech-To-Text";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label m_lblWavFileLocaltion;
+        private System.Windows.Forms.TextBox m_txtBoxWavFileURL;
+        private System.Windows.Forms.Button m_btnSelectFile;
+        private System.Windows.Forms.TextBox m_txtBoxLoopTime;
+        private System.Windows.Forms.Label m_lblLoopTime;
+        private System.Windows.Forms.TextBox m_txtBoxWriteToTextURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label m_lblLanguage;
+        private System.Windows.Forms.ComboBox m_cmbxLanguage;
+        private System.Windows.Forms.Button m_btnSave;
+    }
+}
+
---
+++ WavToText/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.AudioFormat;
+using System.IO;
+
+namespace WavToText
+{
+    public partial class Form1 : Form
+    {
+        private SpeechRecognitionEngine recognitionEngine;
+
+        public Form1()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+            m_cmbxLanguage.SelectedIndex = 3;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void m_btnSave_Click(object sender, EventArgs e)
+        {
+
+            //validation
+
+            if (m_txtBoxWavFileURL.Text == "")
+            {
+                MessageBox.Show("Please enter/ select WAV file");
+                return;
+            }
+
+            if (m_txtBoxLoopTime.Text == "")
+            {
+                MessageBox.Show("Please enter loop time");
+                return;
+            }
+
+            if (m_txtBoxWriteToTextURL.Text == "")
+            {
+                MessageBox.Show("Please enter output textfile");
+                return;
+            }
+
+
+            try
+            {
+                string strLngCode = "en-US";
+                if(m_cmbxLanguage.Text == "English (United States)")
+                {
+                    strLngCode = "en-US";
+                }else if (m_cmbxLanguage.Text == "English (United Kingdom)")
+                {
+                    strLngCode = "en-GB";
+                }
+                else if (m_cmbxLanguage.Text == "German (Germany)")
+                {
+                    strLngCode = "de-DE";
+                }
+                else if (m_cmbxLanguage.Text == "Spanish (Spain)")
+                {
+                    strLngCode = "es-ES";
+                }
+                else if (m_cmbxLanguage.Text == "French (France)")
+                {
+                    strLngCode = "fr-FR";
+                }
+                else if (m_cmbxLanguage.Text == "Japanese (Japan)")
+                {
+                    strLngCode = "ja-JP";
+                }
+                else if (m_cmbxLanguage.Text == "Chinese (China)")
+                {
+                    strLngCode = "zh-CN";
+                }
+                else if (m_cmbxLanguage.Text == "Chinese (Taiwan)")
+                {
+                    strLngCode = "zh-TW";
+                }
+
+
+                recognitionEngine = new SpeechRecognitionEngine(new System.Globalization.CultureInfo(strLngCode));
+                DictationGrammar dg = new DictationGrammar();
+                dg.Name = "Dictation Grammar";
+                recognitionEngine.LoadGrammar(dg);
+                recognitionEngine.SetInputToDefaultAudioDevice();
+                recognitionEngine.SetInputToWaveFile(@"C:\Users\mseelam\Documents\OSR_us_000_0010_8k.wav");
+
+                // recognitionEngine.SetInputToDefaultAudioDevice();  // Gets sound from microphone
+                recognitionEngine.EndSilenceTimeout = new TimeSpan(0, 0, Int16.Parse(m_txtBoxLoopTime.Text));
+                // Following two lines are not needed. They cause error!
+                //recognitionEngine.Recognize();
+                //recognitionEngine.RecognizeAsync();
+                recognitionEngine.SpeechHypothesized += new EventHandler<SpeechHypothesizedEventArgs>(reco_SpeechHypothesized);
+                recognitionEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(reco_SpeechRecognized);
+                recognitionEngine.RecognizeCompleted += new EventHandler<RecognizeCompletedEventArgs>(reco_RecognizeCompleted);
+                recognitionEngine.AudioSignalProblemOccurred += new EventHandler<AudioSignalProblemOccurredEventArgs>(reco_AudioSignalProblemOccurred);
+                recognitionEngine.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(reco_SpeechDetected);
+                recognitionEngine.SpeechRecognitionRejected += new EventHandler<SpeechRecognitionRejectedEventArgs>(reco_SpeechRecognitionRejected);
+                recognitionEngine.RecognizeAsync(RecognizeMode.Single);
+
+                MessageBox.Show("Done.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex);
+            }
+        }
+
+        void reco_AudioSignalProblemOccurred(object sender, AudioSignalProblemOccurredEventArgs e)
+        {
+
+            //using (StreamWriter w = File.AppendText(m_txtBoxWriteToTextURL.Text))
+            //{
+            //    Log("----------------------------------reco_AudioSignalProblemOccurred!---------------------------------------------", w);
+
+            //    Log(e.AudioSignalProblem.ToString(), w);
+            //    Log("----------------------------------reco_AudioSignalProblemOccurred!---------------------------------------------", w);
+
+            //}
+            // m_txtBoxWriteToTextURL.AppendText(e.AudioSignalProblem.ToString());
+            //Console.WriteLine(e.AudioSignalProblem.ToString());
+        }
+
+        void reco_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            //using (StreamWriter w = File.AppendText(m_txtBoxWriteToTextURL.Text))
+            //{
+            //    Log(e.Result.Text, w);
+            //}
+            //m_txtBoxWriteToTextURL.AppendText(e.Result.Text);
+            // Console.WriteLine(e.Result.Text);
+        }
+
+        void reco_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            //m_txtBoxWriteToTextURL.AppendText("Rejected!");
+            // Console.WriteLine("Rejected!");
+
+            using (StreamWriter w = File.AppendText(m_txtBoxWriteToTextURL.Text))
+            {
+                Log("----------------------------------Rejected!---------------------------------------------", w);
+            }
+        }
+
+        void reco_RecognizeCompleted(object sender, RecognizeCompletedEventArgs e)
+        {
+            // m_txtBoxWriteToTextURL.AppendText("Recognition Complete!");
+            //  Console.WriteLine("Recognition Complete!");
+
+            using (StreamWriter w = File.AppendText(m_txtBoxWriteToTextURL.Text))
+            {
+                Log("----------------------------------Recognition Complete!---------------------------------------------", w);
+            }
+        }
+
+        void reco_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            using (StreamWriter w = File.AppendText(m_txtBoxWriteToTextURL.Text))
+            {
+             //   Log("----------------------------------reco_SpeechRecognized!---------------------------------------------", w);
+                Log(e.Result.Text, w);
+               // Log("----------------------------------reco_SpeechRecognized!---------------------------------------------", w);
+
+            }
+            // m_txtBoxWriteToTextURL.AppendText(e.Result.Text);
+            //  Console.WriteLine(e.Result.Text);
+        }
+
+        void reco_SpeechDetected(object sender, SpeechDetectedEventArgs e)
+        {
+            using (StreamWriter w = File.AppendText(m_txtBoxWriteToTextURL.Text))
+            {
+                Log("----------------------------------Speech Detected!---------------------------------------------", w);
+            }
+           // m_txtBoxWriteToTextURL.AppendText("Speech Detected!");
+           // Console.WriteLine("Speech Detected!");
+        }
+
+        private void m_btnSelectFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Multiselect = false;
+                openFileDialog.Filter = "Wave Files(*.wav;*.WAV)|*.wav;*.WAV";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var filePaths = openFileDialog.FileNames;
+                    var fileNames = openFileDialog.SafeFileNames;
+
+                    foreach (int i in Enumerable.Range(0, filePaths.Length))
+                    {
+                        m_txtBoxWavFileURL.Text = filePaths[i];
+                        //m_txtBoxWavFileURL.AppendText(fileNames[i]);
+                        //m_txtBoxWavFileURL.Name = filePaths[i];
+
+                    }
+
+                }
+            }
+        }
+
+        public static void Log(string logMessage, TextWriter w)
+        {
+
+            if(logMessage == "")
+            {
+                return;
+            }
+            //w.Write("\r\nLog Entry : ");
+            //w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+            w.WriteLine("*********************************");
+            w.WriteLine(logMessage);
+            w.WriteLine("*********************************\n");
+
+        }
+
+        private void m_cmbxLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
